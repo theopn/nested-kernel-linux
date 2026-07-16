33ca4b694711c0decd2811ebc13d7bbeb71de811
Theo Park <theo.park.n@gmail.com>
2026-07-16 15:44:58 -0400

    x86/nested_kernel: Implement global PKS initialization and PTE mapping
    This patch integrates PKS enforcement into the Nested Kernel MMU policy:
    1. Provides apply_supervisor_pkey() to flip bits 59:62 in the Page
       Table Entries of the Citadel memory, binding it to PKEY 1.
    2. Initializes CR4.PKS globally via an on_each_cpu() IPI broadcast.
    3. Sets the default PKRS value to 0x8 (Write Disable and Access Disable
       for PKEY 1) globally, ensuring the Citadel remains actively locked
       out from standard Outer Kernel operations across all cores.

--- arch/x86/kernel/nk_mmu.c
+++ arch/x86/kernel/nk_mmu.c
@@ -3,7 +3,9 @@
 #include <asm/paravirt.h>
 #include <asm/pgtable.h>
 #include <asm/set_memory.h>
-
+#include <asm/tlbflush.h>
+#include <asm/processor-flags.h>
+#include <asm/tlb.h>
 phys_addr_t nk_base_phys;
 // Providing the test suite the base address to attack
 //EXPORT_SYMBOL_GPL(nk_base_phys);
@@ -22,6 +24,7 @@ void nk_write_PMD(pmd_t *pmdp, pmd_t pmdval);
 void nk_write(void *dest, const void *src, size_t size);
 void nk_init(void);
 void nk_protect_memory(void);
+void nk_protect_citadel_pks(void);
 
 extern void nk_enter(void *payload, void *arg1, void *arg2);
 extern void nk_exit(void);
@@ -98,4 +101,47 @@ void nk_protect_memory(void) {
     }
     
     pr_info("Nested Kernel: Protected NK memory and PTPs\n");
+    nk_protect_citadel_pks();
+}
+
+#define _PAGE_PKEY_SHIFT _PAGE_BIT_PKEY_BIT0
+
+static int apply_supervisor_pkey(unsigned long addr, int pkey) {
+    unsigned int level; pte_t *pte; pte_t new_pte; unsigned long pte_val;
+    pte = lookup_address(addr, &level);
+    if (!pte) return -ENOENT;
+    pte_val = pte_val(*pte);
+    pte_val &= ~_PAGE_PKEY_MASK;
+    pte_val |= ((unsigned long)(pkey & 0xF)) << _PAGE_PKEY_SHIFT;
+    new_pte = __pte(pte_val);
+    set_pte_atomic(pte, new_pte);
+    return 0;
+}
+
+static void nk_init_cpu_pks(void *info) {
+    /* Enable PKS in CR4 on this specific CPU */
+    cr4_set_bits(X86_CR4_PKS);
+    
+    /* Globally lock the Citadel (Key 1) by default on this CPU
+     * 0x8 = Access Disable (AD) and Write Disable (WD) for PKEY 1
+     */
+#ifndef MSR_IA32_PKRS
+#define MSR_IA32_PKRS 0x6E1
+#endif
+    wrmsrl(MSR_IA32_PKRS, 0x8);
+}
+
+void nk_protect_citadel_pks(void) {
+    unsigned long addr;
+    unsigned long nk_base_virt = (unsigned long)__va(nk_base_phys);
+    unsigned long end = nk_base_virt + nk_size;
+    for (addr = nk_base_virt; addr < end; addr += PAGE_SIZE) {
+        apply_supervisor_pkey(addr, 1);
+    }
+    __flush_tlb_all();
+    
+    /* Broadcast CR4 and PKRS initialization to ALL CPUs simultaneously */
+    on_each_cpu(nk_init_cpu_pks, NULL, 1);
+    
+    printk(KERN_INFO "Nested Kernel: PKS Key 1 applied and locked on all CPUs.\n");
 }
